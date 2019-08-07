9d787ac4a3d2c083f58e1e71d6f7753d32ba24e3
Sylvain <33718890+sylvainkosc@users.noreply.github.com>
2019-08-07 07:09:54 +0200

    Update Tree.java

--- tree-java/Tree/Tree.java
+++ tree-java/Tree/Tree.java
@@ -1,21 +1,35 @@
-package Tree;
+package t_p_Tree;
 
 public class Tree {
+
 	
-	public void add(String s){
-		
-	}
-	public void remove(String s){
-		
-	}
-	public boolean contains (String s) {
-		return false;
-	}
-	public void display(){	
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	}
+	public void add(String s) {
+		
+	}
+	public void remove(String s) {
+		
+	}
+	public boolean contains(String s) {
+		return false;
+		
+	}
+
+	public void display() {
+		System.out.print("[");
+		String first = null;
+		String currentString = first;
+		String comma = "";
+		while(currentString!=null) {
+			System.out.print(comma+currentString.valueOf(0));
+			comma=",";
+			currentString = currentString;
+		}
+		System.out.print("]\n");
 	}
 
 }
